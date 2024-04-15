6815c3428a078582cab8ed8eb6f6ca4bfaeeef36
Yande <113806118+TheYande@users.noreply.github.com>
2024-04-15 17:14:31 -0400

    asfasdfaef

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,35 @@
 <script>
 	import Header from './Header.svelte';
 	import './styles.css';
+	import { page, navigating } from '$app/stores';
 </script>
 
 <div class="app">
 	<Header />
+	
+	
+		{#if $navigating}
+	<div class="navigatoing"><br><br><br><br>navigating to {$navigating.to?.url.pathname}</div>
 
-	<main>
-		<slot />
-	</main>
-
+	
+	
+{/if}
+<main><slot /></main>	
+    
 	<footer>
 		<p>yes yes very website</p>
 	</footer>
 </div>
 
 <style>
+
+	.navigatoing {
+		width: 100%;
+		height: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
 	.app {
 		display: flex;
 		flex-direction: column;
