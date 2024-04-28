7848dab593e2b4c31d422636bd301c797b303196
Yande <113806118+TheYande@users.noreply.github.com>
2024-04-28 01:50:45 -0400

    discord revealing my shenangans

--- src/routes/hmm/+page.server.ts
+++
@@ -1,5 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-
-export function load() {
-	throw redirect(307, 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
-}
---
+++ src/routes/hmmm/+page.svelte
@@ -0,0 +1,3 @@
+<script lang="ts">
+  window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+</script>
